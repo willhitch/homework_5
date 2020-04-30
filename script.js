4384e073b4baa98a9a259ddde3f8fc4f982d0b23
willhitch <hitch.will@gmail.com>
2020-04-30 14:09:55 -0600

    added clear local storage function and button

--- script.js
+++ script.js
@@ -1,16 +1,19 @@
-
 // Variables
 var currentDay = new Date();
 var hour = currentDay.getHours();
 var time;
 var timeBlock;
-var taskArr = [];
 
 // Display Current Day
 $("#currentDay").text(currentDay);
 
 //Set Interval to run workHours Function
 setInterval(workHours, 1000);
+
+//Get local storage
+getTask();
+
+//Click Functions
 
 //Save Click
 $(".saveBtn").on("click", function () {
@@ -23,6 +26,13 @@ $(".saveBtn").on("click", function () {
   var timeOfTask = $(this).parent().parent().attr("id");
 
   localStorage.setItem(timeOfTask, toDo);
+  getTask();
+});
+
+//Clear Click
+$(".clear").on("click", function () {
+  localStorage.clear();
+  getTask();
 });
 
 //Get Local Storage Function
@@ -34,7 +44,6 @@ function getTask() {
 
 //Function to change row classes based on time
 function workHours() {
-  getTask();
   $(".hour").each(function () {
     var time = parseInt($(this).attr("id"));
     var timeBlock = $(this);
