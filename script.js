6508b82618f0bcfad26a4c23d2aa59dd016f7ef4
willhitch <hitch.will@gmail.com>
2020-04-30 13:16:51 -0600

    fixed local storage bugs

--- script.js
+++ script.js
@@ -1,85 +1,52 @@
+
+// Variables
 var currentDay = new Date();
-var hour = 12;
-// currentDay.getHours();
+var hour = currentDay.getHours();
 var time;
-var timeBlock = $(".container").children();
+var timeBlock;
 var taskArr = [];
 
-console.log(timeBlock);
-
+// Display Current Day
 $("#currentDay").text(currentDay);
 
-// setInterval(workHours, 1000);
-workHours();
-
-//Click Events
+//Set Interval to run workHours Function
+setInterval(workHours, 1000);
 
 //Save Click
-
 $(".saveBtn").on("click", function () {
-  var toDo = $(this).parent().siblings().find("textarea").val();
+  var toDo = $(this)
+    .parent()
+    .siblings(".description")
+    .children("textarea")
+    .val();
 
   var timeOfTask = $(this).parent().parent().attr("id");
 
-  console.log(timeOfTask, toDo);
+  localStorage.setItem(timeOfTask, toDo);
 });
 
-function saveTask(timeOfTask, toDo) {
-  localStorage.setItem([timeOfTask, toDo]);
-}
-
-function workHours() {
-  for (var i = 0; i < timeBlock.length; i++) {
-    var time = parseInt(timeBlock[i].id);
-
-    if (time === hour) {
-      timeBlock.toggleClass("present");
-    } else if (time < hour) {
-      timeBlock.toggleClass("past");
-    } else if (time > hour) {
-      timeBlock.toggleClass("future");
-    }
-
-    handleTime(time);
+//Get Local Storage Function
+function getTask() {
+  for (var i = 9; i < 17; i++) {
+    $("#" + i + " textarea").val(localStorage.getItem("" + i + ""));
   }
 }
 
-function handleTime(time) {
-  console.log(time);
-
-  //   if (time === hour) {
-  //     $(this).attr("class", "present");
-  //   } else if (time < hour) {
-  //     $(this).attr("class", "past");
-  //   } else if (time > hour) {
-  //     $(this).attr("class", "future");
-  //   }
-
-  //   switch (time) {
-  //     case time === hour:
-  //       timeBlock.addClass("present");
-  //       timeBlock.removeClass("past future");
-  //       break;
-  //     case time < hour:
-  //       timeBlock.addClass("past");
-  //       timeBlock.removeClass("future present");
-  //       break;
-  //     case time > hour:
-  //       timeBlock.addClass("future");
-  //       timeBlock.removeClass("past present");
-  //       break;
-  //     default:
-  //       break;
-  //   }
-
-  //   if (time === hour) {
-  //     timeClass.addClass("present");
-  //     timeClass.removeClass("past future");
-  //   } else if (time < hour) {
-  //     timeClass.addClass("past");
-  //     timeClass.removeClass("future present");
-  //   } else if (time > hour) {
-  //     timeClass.addClass("future");
-  //     timeClass.removeClass("past present");
-  //   }
+//Function to change row classes based on time
+function workHours() {
+  getTask();
+  $(".hour").each(function () {
+    var time = parseInt($(this).attr("id"));
+    var timeBlock = $(this);
+    if (time < hour) {
+      timeBlock.addClass("past");
+      timeBlock.removeClass("future present");
+    } else if (time > hour) {
+      timeBlock.addClass("future");
+      timeBlock.removeClass("past present");
+    } else {
+      timeBlock.addClass("present");
+      timeBlock.removeClass("past future");
+    }
+  });
 }
